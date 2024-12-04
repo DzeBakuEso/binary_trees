364fb496f9a302a99d152ec2f8a69db8be17d62c
DzeBakuEso <dzeblebaku@gmail.com>
2024-12-04 14:38:44 -0800

    8-binary_tree_postorder.c

--- binary_tree_node.c
+++ binary_tree_node.c
@@ -2,23 +2,23 @@
 
 /**
  * binary_tree_node - Creates a binary tree node
+ * @parent: Pointer to the parent node
+ * @value: Value to put in the new node
  *
- * @parent: Pointer to the parent node of the node to create
- * @value: Value to store in the new node
- *
- * Return: Pointer to the new node, or NULL if it failed
+ * Return: Pointer to the new node, or NULL on failure
 */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-    binary_tree_t *node = malloc(sizeof(binary_tree_t));
+	binary_tree_t *new_node;
 
-    if (node == NULL)
-        return (NULL);
+	new_node = malloc(sizeof(binary_tree_t));
+	if (new_node == NULL)
+		return (NULL);
 
-    node->n = value;
-    node->left = NULL;
-    node->right = NULL;
-    node->parent = parent;
+	new_node->n = value;
+	new_node->parent = parent;
+	new_node->left = NULL;
+	new_node->right = NULL;
 
-    return (node);
+	return (new_node);
 }
